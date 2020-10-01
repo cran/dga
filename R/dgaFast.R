6e0f4acaee24329cee73f0940c5a3440205885e0
olivierbinette <olivier.binette@gmail.com>
2020-10-01 18:37:47 -0400

    Pass check

--- R/dgaFast.R
+++ R/dgaFast.R
@@ -1,7 +1,10 @@
-#' Multiple systems estimation using decomposable graphical models.
+#' dgaFast: Multiple systems estimation using decomposable graphical models.
 #'
 #' @description
 #' This is an efficient re-implementation of the \code{dga} package of James Johndrow, Kristian Lum and Patrick Ball (2015) which implements Madigan and York (1997).
 #'
-#'
+#' @docType package
+#' @name dgaFast
+#' @import Rcpp
+#' @useDynLib dgaFast, .registration=TRUE
 "_PACKAGE"
--- R/plot.R
+++ R/plot.R
@@ -1,10 +1,10 @@
 #' @export
 plotPosteriorN <- function(weights, N, main = NULL) {
-  plot(N, colSums(weights), type = "l", col = "black", lwd = 3, ylab = "Posterior Probability of N", xlab = "N", ylim = c(0, 1.25 * max(colSums(weights))))
-  title(main)
+  graphics::plot(N, colSums(weights), type = "l", col = "black", lwd = 3, ylab = "Posterior Probability of N", xlab = "N", ylim = c(0, 1.25 * max(colSums(weights))))
+  graphics::title(main)
   wts <- rowSums(weights)
   for (i in 1:nrow(weights)) {
-    lines(N, weights[i, ], lwd = wts[i] * 3, lty = "dashed")
+    graphics::lines(N, weights[i, ], lwd = wts[i] * 3, lty = "dashed")
   }
-  legend("topright", legend = c("Averaged Post. Prob.", "Post. Prob. By Model"), lty = c(1, 2), cex = .75)
+  graphics::legend("topright", legend = c("Averaged Post. Prob.", "Post. Prob. By Model"), lty = c(1, 2), cex = .75)
 }
