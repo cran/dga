7cb1755bdb3b33d04769c90de6c96154770a87af
olivierbinette <olivier.binette@gmail.com>
2020-10-01 17:18:54 -0400

    Add package description

---
+++ R/dgaFast.R
@@ -0,0 +1,7 @@
+#' Multiple systems estimation using decomposable graphical models.
+#'
+#' @description
+#' This is an efficient re-implementation of the \code{dga} package of James Johndrow, Kristian Lum and Patrick Ball (2015) which implements Madigan and York (1997).
+#'
+#'
+"_PACKAGE"
